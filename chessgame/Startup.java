081300d39bb288e31e25e39429454ed55cbd8088
Tressos-Aristomenis <34586179+Tressos-Aristomenis@users.noreply.github.com>
2018-10-04 17:58:48 +0300

    Update Startup.java

--- chessgame/Startup.java
+++ chessgame/Startup.java
@@ -19,7 +19,7 @@ public class Startup extends Application {
         Scene scene = new Scene(root);
         stage.setResizable(false);
         stage.setTitle("A chess game");
-		stage.getIcons().add(new Image(".resources/Chess_nlt60.png"));
+	stage.getIcons().add(new Image(".resources/Chess_nlt60.png"));
         stage.setScene(scene);
         stage.show();
     }
