8001c6851d60ec864e2afa124e8efa4ee6a8e3e1
Tressos-Aristomenis <ninjacawboyzz@gmail.com>
2018-10-04 17:57:47 +0300

    added game icon

--- chessgame/Startup.java
+++ chessgame/Startup.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -18,6 +19,7 @@ public class Startup extends Application {
         Scene scene = new Scene(root);
         stage.setResizable(false);
         stage.setTitle("A chess game");
+		stage.getIcons().add(new Image(".resources/Chess_nlt60.png"));
         stage.setScene(scene);
         stage.show();
     }
